434fe9016760d75e855680081250748973543f86
Gareton <37975712+Gareton@users.noreply.github.com>
2024-02-18 22:28:02 +0300

    Fix spice-a

--- spice-a/byol_a.py
+++ spice-a/byol_a.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-sys.path.append(os.path.join(script_dir, 'byol-a', 'v2'))
+sys.path.append(os.path.join(repo_dir, 'byol-a', 'v2'))
 
 from byol_a2.common import load_yaml_config
 from byol_a2.models import AudioNTT2022, load_pretrained_weights
@@ -11,7 +11,7 @@ from byol_a2.augmentations import PrecomputedNorm
 from byol_a2 import augmentations
 from copy import deepcopy
 
-byola_cfg = load_yaml_config(os.path.join(script_dir, 'byol-a', 'v2', 'config_v2.yaml'))
+byola_cfg = load_yaml_config(os.path.join(repo_dir, 'byol-a', 'v2', 'config_v2.yaml'))
 
 def get_byola_cfg():
     return deepcopy(byola_cfg)
@@ -23,7 +23,7 @@ def get_frozen_pretrained_byola(dataset_stats, device, only_highlevel_features=F
     pretrained_byola = AudioNTT2022(d=byola_cfg.feature_d, n_mels=byola_cfg.n_mels)
     load_pretrained_weights(
         pretrained_byola, 
-        os.path.join(script_dir, 'byol-a', 'v2', 'AudioNTT2022-BYOLA-64x96d2048.pth')
+        os.path.join(repo_dir, 'byol-a', 'v2', 'AudioNTT2022-BYOLA-64x96d2048.pth')
     )
     pretrained_byola.eval()
 
