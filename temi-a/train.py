7c3f2784b9684b88af5c27fe4493642f272577a7
Gareton <37975712+Gareton@users.noreply.github.com>
2024-02-28 15:30:39 +0300

    Add schedulers

--- temi-a/train.py
+++ temi-a/train.py
@@ -11,8 +11,7 @@ from losses import MultiHeadTEMILoss
 from metrics import accuracy_with_reassignment, nmi_geom, standartify_clusters
 
 @torch.no_grad()
-def update_teachers(students, teachers):
-    momentum = 0.996
+def update_teachers(students, teachers, momentum):
     for teacher_param, student_param in zip(teachers.parameters(), students.parameters()):
         teacher_param.data.mul_(momentum).add_(student_param.detach().data, alpha=1-momentum)
     
@@ -42,17 +41,35 @@ def eval_head(teachers, teacher_idx, embeds, labels, batch_size):
     print(Counter(pseudo_labels))
     return acc, nmi
 
-def train_epoch(optimizer, students, teachers, embeds, knn_indices, epoch, batch_size, criterion, n_heads):
+def train_epoch(
+    optimizer, 
+    students, 
+    teachers, 
+    lr_schedule,
+    wd_schedule,
+    momentum_schedule,
+    embeds, 
+    knn_indices, 
+    epoch, 
+    batch_size, 
+    criterion, 
+    n_heads
+):
     students.train()
     teachers.train()
-    n_its = embeds.shape[0] // batch_size
-    bar = tqdm(range(n_its), desc=f"Train epoch {epoch}")
+    n_its_per_epoch = embeds.shape[0] // batch_size
+    bar = tqdm(range(n_its_per_epoch), desc=f"Train epoch {epoch}")
     n_embeds = embeds.shape[0]
     n_neighbours = knn_indices.shape[1]
     epoch_losses = [0] * n_heads 
 
-    for _ in bar:
-        optimizer.zero_grad()
+    for it in bar:
+        # update weight decay and learning rate according to their schedule
+        it = n_its_per_epoch * epoch + it  # global training iteration        
+        for i, param_group in enumerate(optimizer.param_groups):
+            param_group["lr"] = lr_schedule[it]
+            if i == 0:  # only the first group is regularized
+                param_group["weight_decay"] = wd_schedule[it]
 
         # Sample random knn pairs for batch
         samples = torch.randint(size=(batch_size,), high=n_embeds)
@@ -67,26 +84,78 @@ def train_epoch(optimizer, students, teachers, embeds, knn_indices, epoch, batch
         # Calculate loss
         losses = criterion(sprobs, tprobs)
         avg_loss = sum(losses) / len(losses)
-        epoch_losses = [l1 + l2.item() / n_its for l1, l2 in zip(epoch_losses, losses)]
+        epoch_losses = [l1 + l2.item() / n_its_per_epoch for l1, l2 in zip(epoch_losses, losses)]
 
         # Backpropagation for students
+        optimizer.zero_grad()
         avg_loss.backward()
         optimizer.step()
 
         # Update teachers
-        update_teachers(teachers=teachers, students=students)
+        update_teachers(teachers=teachers, students=students, momentum=momentum_schedule[it])
 
     return epoch_losses
 
+def get_params_groups(model):
+    regularized = []
+    not_regularized = []
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue
+        # we do not regularize biases nor Norm parameters
+        if name.endswith(".bias") or len(param.shape) == 1:
+            not_regularized.append(param)
+        else:
+            regularized.append(param)
+    return [{'params': regularized}, {'params': not_regularized, 'weight_decay': 0.}]
+
+def cosine_scheduler(base_value, final_value, epochs, niter_per_ep, warmup_epochs=0, start_warmup_value=0):
+    warmup_schedule = np.array([])
+    warmup_iters = warmup_epochs * niter_per_ep
+    if warmup_epochs > 0:
+        warmup_schedule = np.linspace(start_warmup_value, base_value, warmup_iters)
+
+    iters = np.arange(epochs * niter_per_ep - warmup_iters)
+    schedule = final_value + 0.5 * (base_value - final_value) * (1 + np.cos(np.pi * iters / len(iters)))
+
+    schedule = np.concatenate((warmup_schedule, schedule))
+    assert len(schedule) == epochs * niter_per_ep
+    return schedule
+
 def train(args, students, teachers, embeds, knn_indices):
     labels = load_labels(args.labels_path) 
-    optimizer = torch.optim.AdamW(students.parameters(), lr=args.lr, weight_decay=1e-4)
+    # Optimizer
+    optimizer = torch.optim.AdamW(get_params_groups(students))
+    # Loss function
     criterion = MultiHeadTEMILoss(
         n_heads=args.n_heads, 
         n_classes=args.n_clusters, 
         momentum=0.99,
         beta=args.beta
     )
+    # Schedulers
+    bs_factor = args.batch_size / 256.
+    its_per_epoch = embeds.shape[0] // args.batch_size
+    lr_schedule = cosine_scheduler(
+        args.lr * bs_factor,  # linear scaling rule
+        args.lr * bs_factor,
+        args.n_epochs, its_per_epoch,
+        warmup_epochs=args.warmup_epochs,
+    )
+    wd_schedule = cosine_scheduler(
+        args.weight_decay,
+        args.weight_decay,
+        args.n_epochs, its_per_epoch 
+    )
+    # momentum parameter is increased to 1. during training with a cosine schedule
+    momentum_schedule = cosine_scheduler(
+        args.momentum_teacher, 
+        args.momentum_teacher,
+        args.n_epochs, 
+        its_per_epoch
+    )
+
+    # Train loop
     best_head_idx = 0
 
     for i in range(args.n_epochs):
@@ -94,6 +163,9 @@ def train(args, students, teachers, embeds, knn_indices):
             optimizer=optimizer,
             students=students,
             teachers=teachers,
+            lr_schedule=lr_schedule,
+            wd_schedule=wd_schedule,
+            momentum_schedule=momentum_schedule,
             embeds=embeds,
             knn_indices=knn_indices,
             epoch=i,
@@ -196,6 +268,18 @@ if __name__ == "__main__":
     parser.add_argument(
         '--beta', default=0.6, type=float,
         help="Beta used as a power inside PMI"
+    )
+    parser.add_argument(
+        '--momentum_teacher', default=0.996, type=float,
+        help="Momentum for updating teachers using EMA"
+    )
+    parser.add_argument(
+        '--weight_decay', default=1e-4, type=float,
+        help="Weight decay for optimizer"
+    )
+    parser.add_argument(
+        '--warmup_epochs', default=20, type=int,
+        help="Number of warmup epochs"
     )
     main(parser.parse_args())
 
